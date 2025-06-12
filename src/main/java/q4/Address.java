6f5bbb755267367dee74a41d106d0ff89306981c
Rafaelrs21 <santireis14@gmail.com>
2025-06-12 16:28:29 -0300

    first commit

--- src/main/java/q4/Address.java
+++ src/main/java/q4/Address.java
@@ -6,7 +6,6 @@ public class Address {
     private String state;
     private String zipCode;
 
-    // Construtor
     public Address(String street, String city, String state, String zipCode) {
         this.street = street;
         this.city = city;
@@ -14,16 +13,10 @@ public class Address {
         this.zipCode = zipCode;
     }
 
-    // Getters
     public String getStreet() { return street; }
     public String getCity() { return city; }
     public String getState() { return state; }
     public String getZipCode() { return zipCode; }
 
-    // Setters (opcional)
-    public void setStreet(String street) { this.street = street; }
-    public void setCity(String city) { this.city = city; }
-    public void setState(String state) { this.state = state; }
-    public void setZipCode(String zipCode) { this.zipCode = zipCode; }
 }
 
